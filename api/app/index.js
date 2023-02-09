1aaaeed66fb1d3ff0ccf326333a6115716f834f0
Elena Dragunova <elena.dragunova@saritasa.com>
2023-02-09 13:49:47 +0500

    Add cors headers

--- api/app/index.js
+++ api/app/index.js
@@ -3,34 +3,26 @@ const http = require("http");
 const host = '0.0.0.0';
 const port = 8000;
 
-const books = JSON.stringify([
-    { title: "The Alchemist", author: "Paulo Coelho", year: 1988 },
-    { title: "The Prophet", author: "Kahlil Gibran", year: 1923 }
-]);
-
-const authors = JSON.stringify([
-    { name: "Paulo Coelho", countryOfBirth: "Brazil", yearOfBirth: 1947 },
-    { name: "Kahlil Gibran", countryOfBirth: "Lebanon", yearOfBirth: 1883 }
-]);
-
 const requestListener = function (req, res) {
-    res.setHeader("Content-Type", "application/json");
-    switch (req.url) {
-        case "/books":
-            res.writeHead(200);
-            res.end(books);
-            break;
-        case "/authors":
-            res.writeHead(200);
-            res.end(authors);
-            break;
-        case "/auth/login":
-            res.writeHead(200);
-            res.end('Login request');
-            break;
-        default:
-            res.writeHead(404);
-            res.end(JSON.stringify({error:"Resource not found"}));
+    const headers = {
+        "Access-Control-Allow-Origin": "*",
+        'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE',
+        'Access-Control-Allow-Headers': 'Content-Type',
+    }
+    if (req.method === 'OPTIONS') {
+        res.writeHead(200, headers);
+        res.end();
+    } else {
+        res.setHeader("Content-Type", "application/json");
+        switch (req.url) {
+            case "/auth/login":
+                res.writeHead(200, headers);
+                res.end('Login request');
+                break;
+            default:
+                res.writeHead(404, headers);
+                res.end(JSON.stringify({error:"Resource not found"}));
+        }
     }
 };
 
