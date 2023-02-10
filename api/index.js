c3632c9e1586292c9dc058bc2910e874c06ebbb5
Elena Dragunova <elena.dragunova@saritasa.com>
2023-02-10 12:39:40 +0500

    Add pg module, establish connection to db

--- api/index.js
+++ api/index.js
@@ -30,3 +30,28 @@ const server = http.createServer(requestListener);
 server.listen(port, host, () => {
     console.log(`Server is running on http://${host}:${port}`);
 })
+
+const { Pool } = require('pg')
+ 
+const pool = new Pool({
+  host: 'db',
+  user: 'postgres',
+  password: 'postgres',
+  database: 'database',
+  max: 20,
+  idleTimeoutMillis: 30000,
+  connectionTimeoutMillis: 2000,
+})
+
+pool.connect((err, client, release) => {
+    if (err) {
+      return console.error('Error acquiring client', err.stack)
+    }
+    client.query('SELECT * from users', (err, result) => {
+      release()
+      if (err) {
+        return console.error('Error executing query', err.stack)
+      }
+      console.log(result.rows)
+    })
+  })
